01e7bba10a71f84a086c9f2a7c3e97bebdd52e29
EtienneSafa <e.safa@protonmail.com>
2021-11-15 22:24:45 +0100

    first commit

---
+++ Onyx4Oryx.py
@@ -0,0 +1,104 @@
+from time import sleep as sleep
+
+# Features' status last checkup date
+#   check list: 
+#   - nb of keystrokes in macros
+date = '2020-11-19'
+
+
+
+f = open('layouts.txt','r', encoding="utf-8")
+
+
+
+
+# Fetch layouts in layouts.txt using "||" as a separator
+layouts = {}
+
+for line in f:
+    Line = line.split('\n')[0]
+    layouts[Line.split('=',1)[0]] = Line.split('=',1)[1].split('||')
+
+knownLayouts = list(layouts.keys())
+knownLayouts.remove('English (qwerty)')
+knownLayouts.sort()
+
+
+
+
+# Inquire for user choice
+print ("\n   (⌐■_■)  >>  Onyx4Oryx will translate the keys you need for your macros if you're working on a non-qwerty keyboard.\n           >>  Here are the layouts currently available at development stage:\n")
+
+for layout in knownLayouts:
+    print(f"          [{knownLayouts.index(layout)}]  {layout}")
+
+
+sleep(2)
+
+while True:
+
+    choice = input("\n\n ╰(*°▽°*)╯ >>  Which layout wou'd you like to translate?\n\n       you >>  ")
+
+
+    while True:
+        if isinstance(choice, int):
+            if choice in range(0,len(knownLayouts)-1):
+                break
+            else:
+                choice = int(input("\n\n  '￣へ￣  >>  That's not in the list...\n\n       you >>  "))
+                pass
+        else:
+            choice = int(input("\n\n o(≧口≦)o  >>  That's not a number...\n\n       you >>  "))
+            pass
+
+    usrLayout=knownLayouts[choice]
+
+
+
+    while True:
+        sequence = input(f"\n   (⌐■_■)  >>  OK for \"{usrLayout}\". Please enter your sequence (only simple keystrokes: don't use any modifiers as you'll be able to tick them on Oryx)\n\n       you >>  ")
+
+        while True:
+            if len(sequence) > 5:
+                sequence = input(f"\nㄟ( ▔, ▔ )ㄏ >> Oryx can only take 5 keystrokes as of now ({date})\n\n       you >>  ")
+            else:
+                break
+
+        S = list(sequence)
+
+        sleep(0.33)
+
+        print("\n\n (￣y▽￣)╭ >>  Here it is!")
+
+
+        for letter in S:
+        #    idx = sequence.find(letter)
+            for row in layouts[usrLayout]:
+                rowIdx = layouts[usrLayout].index(row)
+                if row.find(letter) != -1:
+                    row = list(layouts[usrLayout][rowIdx])
+                    ltrIdx = row.index(letter)
+                    l = list(layouts['English (qwerty)'][rowIdx])
+                    print('       ', letter, '-->', l[ltrIdx])
+        
+    
+        sleep(3)
+
+
+        repeat = input('\n\n   (⌐■_■)  >>  Would you like to input a new sequence? (Yes/No)\n           >>  You can also change the layout by typing "change layout" (don\'t make any typos...)\n\n       you >>  ')
+        
+        if repeat in ['yes', 'Yes', 'y', 'Y', 'YES']:
+            pass
+        elif repeat in ['no','No','n','N','NO','q','Q','quit']:
+            print("\n\nSee ya'!  ¯\_(ツ)_/¯\n\n")
+            break
+        elif repeat in ["change layout", "Change layout"]:
+            break
+        else:
+            print ('\n   (⌐■_■)  >>  What was that?')
+            pass
+    
+    if repeat in ["change layout", "Change layout"]:
+        pass
+    else:
+        break
---
+++ readme.md
@@ -0,0 +1,78 @@
+# Readme
+
+## Onyx4Oryx
+
+Onyx4Oryx is a tiny bit of code dedicated to non-qwerty users of Moonlander and Ergodox's ZSA split keyboards.
+
+It's mission is to facilitate the use of macros, currently only compatible with qwerty keyboards, by translating your foreign keystrokes into qwerty keystrokes so you know what to choose from the menu :-)
+
+**Hope it's helpfull, have fun !**
+
+______
+
+## Contributing
+
+### How to
+
+Onyx4Oryx could use some help in the layout's collection: **don't hesitate and send your layout as a _one-line string_ according to the pattern from `layouts.txt` if you can't find yours!**
+
+Here is how to format your string:
+
+* Names may contain spaces
+* `(xxxx)` follows to be more explicit about the layout
+* `"="` (equal sign) follows without spaces
+* `row-without-spaces` follows without spaces
+* use `"||"` (double pipe)
+
+**example of formatted layout string from `layouts.txt`:**
+
+> `English (qwerty)=`1234567890-=||qwertyuiop[]||asdfghjkl;'#||\zxcvbnm,./`
+
+> * 1st row = `1234567890-=`
+> * 2nd row = `qwertyuiop[]`
+> * 3rd row = `asdfghjkl;'#`
+> * 4th row = `\zxcvbnm,./`
+
+
+### WIP
+```
+[X] Belgian
+[X] Bépo 
+[ ] Brazilian 
+[X] Canadian multilingual
+[ ] Czech  
+[ ] Danish 
+[X] English - Great Britain
+[ ] Estonian 
+[X] French 
+[X] French Canadian
+[X] German 
+[ ] Hungarian 
+[ ] Icelandic 
+[ ] Italian 
+[ ] Japanese 
+[ ] Kazakh 
+[ ] Korean 
+[ ] Lithuanian 
+[ ] Nordic
+[ ] Norwegian 
+[ ] Polish (programmer's)
+[ ] Portuguese 
+[ ] Portuguese keycodes macOS
+[ ] Romanian 
+[ ] Russian 
+[ ] Slovenian 
+[ ] Spanish 
+[ ] Spanish-Latin American
+[ ] Swedish
+[ ] Swiss French
+[ ] Swiss German
+[ ] Ukranian 
+[ ] US - International
+```
+
+_________
+
+## Licence
+
+This work is under the **CC-BY-NC-SA** licence.
